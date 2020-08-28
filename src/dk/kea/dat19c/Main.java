5290a103f276d9cf6183a14252cf97cc813d2604
JarlTuxen <jarl.tuxen@gmail.com>
2020-08-28 10:00:24 +0200

    polimorfi

--- src/dk/kea/dat19c/Main.java
+++ src/dk/kea/dat19c/Main.java
@@ -1,9 +1,14 @@
 package dk.kea.dat19c;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// først en maskine
+	    /*
+	    // først en maskine
+
         Machine m = new Machine();
         System.out.println(m);
         m.turnOn();
@@ -16,7 +21,23 @@ public class Main {
         System.out.println(m);
 
         // så en dvd-spiller
+        m = new DVD();
+        System.out.println(m);
+        m.turnOn();
+        System.out.println(m);
+        */
 
+        //opret arraylist med apparater
+        List<Machine> machines =new ArrayList<>();
+        machines.add(new Machine());
+        machines.add(new Radio());
+        machines.add(new DVD());
+
+        //loop igennem og udskriv
+        for (Machine ma: machines){
+            ma.turnOn();
+            System.out.println(ma);
+        }
 
     }
 }
